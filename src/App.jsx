96f64880d6a2b1d622f4d3c98675e59ff18898e4
Sam Odulaja <samuelod@buffalo.edu>
2023-04-04 12:34:19 -0400

    Finish app syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import team from "./data/team";
+import TicketGallery from "./containers/TicketGallery/TicketGallery";
 
 function App() {
   return (
     <div className="app">
       <h1 className='ticket-tracker__heading'>Ticket Tracker</h1>
-      <main className='ticket-tracker__cards'></main>
-
+      <main className='ticket-tracker__cards'>
+        <TicketGallery EmployeeArr={team} />
+      </main>
     </div>
   );
 };
