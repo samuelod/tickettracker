1d2fa5d0a15b7aa37bf9a0cf9c1d21bbaac95d34
Sam Odulaja <samuelod@buffalo.edu>
2023-04-04 11:36:48 -0400

    another commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className='ticket-tracker__heading'>Ticket Tracker</h1>
+      <main className='ticket-tracker__cards'></main>
+
+    </div>
+  );
+};
+
+export default App;
